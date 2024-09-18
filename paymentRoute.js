0e958df3b6df0ed0ba638e278f301f12e37438a9
KrisChe8 <krischernobai@gmail.com>
2024-09-18 15:18:50 +0300

    cors

--- paymentRoute.js
+++ paymentRoute.js
@@ -8,6 +8,14 @@ const stripe = require("stripe")(process.env.SECRET_KEY);
 console.log(process.env.SECRET_KEY);
 
 router.post("/intents", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: req.body.amount, // Integer in pence
