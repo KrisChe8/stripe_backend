8351e1365c432959df58142a5c694311d1a7ed82
KrisChe8 <krischernobai@gmail.com>
2024-09-18 15:32:15 +0300

    cors

--- index.js
+++ index.js
@@ -3,41 +3,22 @@ const paymentRoute = require("./paymentRoute");
 require("dotenv").config({
   path: `${__dirname}/.env`,
 });
+
 const cors = require("cors");
-const corsOrigin = {
-  origin: "http://localhost:5173", //or whatever port  frontend is using
-  credentials: true,
-  optionSuccessStatus: 200,
-};
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-app.use(cors(corsOrigin));
+// Enable CORS for the frontend
+const corsOptions = {
+  origin: "http://localhost:5173", // frontend's origin
+  credentials: true, // if you need to allow cookies or authentication headers
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
 
 app.use("/payments", paymentRoute);
 
