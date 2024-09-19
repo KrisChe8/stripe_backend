39e5c3c10ae6029265f0918348588d49783109ec
KrisChe8 <krischernobai@gmail.com>
2024-09-19 15:26:36 +0300

    change cors allow link

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Enable CORS for the frontend
 const corsOptions = {
-  origin: "http://localhost:5173", // frontend's origin
+  origin: "https://event-board-psi.vercel.app/*", // frontend's origin
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true, // if you need to allow cookies or authentication headers
   optionsSuccessStatus: 204,
