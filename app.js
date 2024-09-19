c7c4312e0ecd72369d865a18e5f0dc284dafd92c
KrisChe8 <krischernobai@gmail.com>
2024-09-19 15:30:30 +0300

    allow all url in cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Enable CORS for the frontend
 const corsOptions = {
-  origin: "https://event-board-psi.vercel.app/*", // frontend's origin
+  origin: "*", // frontend's origin
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true, // if you need to allow cookies or authentication headers
   optionsSuccessStatus: 204,
